f5c3934d05cdc77c2ba1e557a6c474d9f7ed971f
hamogu <moritz.guenther@gmx.de>
2019-02-03 22:02:25 -0500

    Set up abstract deamon

---
+++ abstract_deamon/deamon.py
@@ -0,0 +1,113 @@
+# https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
+import sys
+import os
+import smtplib
+import subprocess
+from warnings import warn
+from tempfile import TemporaryDirectory
+from email.message import EmailMessage
+import re
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from astropy.table import Table
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+sys.path.append('../pagepy')
+from abstracts import process_google_form_value
+
+
+def set_timestamp(sheet, col, row, status=''):
+    '''Put timestamp into a specific cell in a Google sheet.
+
+    The timestamp is written in the same string format that Google uses
+    by default to mark the time a Google from entry was written.
+
+    Parameters
+    ----------
+    sheet : gspread worksheet
+    col : int
+        column number (0 based as in Python)
+    row : int
+        row number (0 based as in Python)
+    status : string
+        A prefix to the time stamp. This can be used to
+        mark a cell that is currently processed.
+    '''
+    val = datetime.now().strftime('%m/%d/%Y %H:%M:%S')
+    sheet.update_cell(row + 1, col + 1, status + val)
+
+
+def send_conf_email(dat):
+    if len(dat) != 1:
+        raise ValueError('Table with data for email needs to have exactly one row.')
+    with open('../../gmail.txt') as f:
+        password = f.read()
+    password = password[:-1]
+    process_google_form_value(dat, autoacceptposters=True)
+
+    # Create the container email message.
+    msg = EmailMessage()
+    msg['From'] = 'conferenceorgbot+tasc5kasc12@gmail.com'
+    msg['To'] = dat['Email Address']
+    msg['Subject'] = 'TASC5/KASC12 Abstract submission - proofs'
+    emailtext = env.get_template('abstract_email.txt')
+    msg.set_content(emailtext.render(dat=dat[0]))
+    msg.preamble = 'HTML files are attached, but it seems your email reader is not MIME aware.\n'
+
+    htmltext = env.get_template('single_abstract.html')
+    msg.add_attachment(htmltext.render(row=dat[0]),
+                       subtype='html',
+                       filename='abstract.html')
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as s:
+        s.ehlo()
+        s.starttls()
+        s.login(msg['From'].split('+')[0], password)
+        s.send_message(msg)
+    print('{}: Send email to: {}'.format(datetime.now().strftime('%m/%d/%Y %H:%M:%S'),
+                                             msg['To']))
+
+
+env = Environment(loader=FileSystemLoader(['../templates']),
+                  autoescape=select_autoescape(['html']))
+
+parse_sheet_timestamp = re.compile("(?P<month>[0-9]+)/(?P<day>[0-9]+)/(?P<year>[0-9]+) (?P<hour>[0-9]+):(?P<minute>[0-9]+):(?P<second>[0-9]+)")
+
+# pip install --upgrade google-auth-oauthlib
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('../../tasc5kasc12-6b096b90207b', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("Abstract submission (Responses)").sheet1
+
+list_of_lists = sheet.get_all_values()
+g_sheet = Table(rows=list_of_lists[1:], names=list_of_lists[0])
+
+ind_conf = list_of_lists[0].index('confemail')
+
+for i, ts in enumerate(g_sheet['Timestamp']):
+    match = parse_sheet_timestamp.match(ts)
+    # The following loop is not written in the most concise way,
+    # but is a way that makes it easy to follow the logic
+    if match:
+        # New entry. No conf email send yet or timestamp removed
+        # by hand in sheet to trigger resending
+        if g_sheet['confemail'][i] == '':
+            set_timestamp(sheet, ind_conf, i + 1, status='Working on ')
+            send_conf_email(g_sheet[[i]])
+            set_timestamp(sheet, ind_conf, i + 1)
+        else:
+            matchconf = parse_sheet_timestamp.match(g_sheet['confemail'][i])
+            if matchconf is None:
+                warn('Cannot parse time for confemail: {}'.format(g_sheet['confemail'][i]))
+            else:
+                # entry has been modified
+                if (datetime(**{k: int(v) for k, v in match.groupdict().items()}) >
+                   datetime(**{k: int(v) for k, v in matchconf.groupdict().items()})):
+                    set_timestamp(sheet, ind_conf, i + 1, status='Working on ')
+                    send_conf_email(g_sheet[[i]])
+                    set_timestamp(sheet, ind_conf, i + 1)
---
+++ pagepy/abstracts.py
@@ -0,0 +1,210 @@
+import json
+from datetime import datetime
+import numpy as np
+import csv
+from astropy.table import Table, Column, join
+
+dates = {'': [2018, 8, 28],  # Posters don't have a date. They should be displayed last.
+         'TBA': [2018, 7, 28],
+         'Sun': [2018, 7, 21],
+         'Mon': [2018, 7, 22],
+         'Tue': [2018, 7, 23],
+         'Wed': [2018, 7, 24],
+         'Thu': [2018, 7, 25],
+         'Fri': [2018, 7, 26],
+         'Sat': [2018, 7, 27]}
+'''Mapping between 3-character day code and date'''
+
+
+def parse_day_time(day, timestr, end=False):
+    '''Turn day and time string into a datetime object.
+
+    If time is empty or "TBA", the return time is late in the
+    afternoon to that not yet assigned talks get printed last
+    in the list of sorted talks. A default for ``day`` can be set
+    by adding the appropriate values to the look-up table ``dates``.
+
+    Parameters
+    ----------
+    day : string
+        index for lookup in ``dates`` table
+    timestr : string
+        string for time in the form "14:30 - 15:15".
+    end : bool
+        Return end time of event (default is start time)
+
+    Returns
+    -------
+    datetime : `datetime.datetime`
+        Start time of event as `datetime.datetime` object.
+    '''
+    if ((day is np.ma.masked) or (timestr is np.ma.masked) or (day is None)
+        or (timestr is None)):
+        return None
+    i = 1 if end else 0
+    if timestr in ['', 'TBA']:
+        time = ['19', '00']
+    else:
+        time = timestr.split('-')[i].split(':')
+    d = dates[day[:3]]
+    return datetime(d[0], d[1], d[2], int(time[0].strip()), int(time[1].strip()))
+
+contribtype = {'': 'not assigned',
+               'poster': 'poster',
+               'invited': 'invited review',
+               'contributed': 'contributed talk'}
+
+
+def combine_affils(affils):
+    if len(affils) == 1:
+        return affils[0]
+    else:
+        return '; '.join(affils)
+
+
+def loctime(row):
+    if row['type'] == 'poster':
+        return 'poster number: {}'.format(row['poster number'])
+    elif (row['type'] == 'invited') or (row['type'] == 'contributed'):
+        if ('day' in row.colnames) and (row['day'] != ''):
+            d = row['day']
+        else:
+            d = 'TBA'
+        if ('time' in row.colnames) and (row['time'] != ''):
+            t = row['time']
+        else:
+            t = 'time to be announced'
+        return '{}, {}'.format(d, t)
+    else:
+        return ''
+
+# missing in list below: mark invited talks
+def write_json_abstracts(abstr):
+    data = {'data': []}
+    for row in abstr:
+        data['data'].append({'type': contribtype[row['type']],
+                             'author': row['First author'],
+                             'authorlist': row['Authors'],
+                             'affiliations': row['affiliations'],
+                             'abstract': '<p class="abstract">' + row['Abstract'].replace('\n\n', '</p><p class="abstract">') + '</p>',
+                             'title': row['Title'],
+                             'authoremail': "<a href='mailto:{0}'>{0}</a>".format(row['Email Address']) if row['Publish first author contact information?'] else '--',
+                             'link': '<a href="{0}">{0}</a>'.format(row['Link to electronic material']) if row['Link to electronic material'] else '--',
+                             'loctime': loctime(row),
+                             'index': row['index'],
+                             })
+    with open('data/abstracts.json', 'w') as fp:
+        json.dump(data, fp)
+
+
+def process_google_form_value(tab, **kwargs):
+    '''Process the values collected in the Google abstract form
+
+    Some of the form fields require some processing to get them into
+    the forms that are most useful in building the abstract booklet
+    and website, for example, the authorlist needs to be broken from a
+    single long string into t list of strings.
+
+    This function add new colums to a table ``tab``.
+    '''
+    tab['authorlist'] = [r.split('\n') for r in tab['Authorlist']]
+    tab['First author'] = [r[0].split('(')[0].strip() for r in tab['authorlist']]
+    tab['affiliations'] = [r.split('\n') for r in tab['Affiliations']]
+    tab['affiliations'] = [combine_affils(r) for r in tab['affiliations']]
+    tab['binary_time'] = [parse_day_time(r['day'], r['time']) for r in tab]
+    tab['typepreference'] = [n.split(' ')[0] for n in tab['Presentation type']]
+
+    team = Column(length=len(tab), dtype='<U140')
+    for i, f in enumerate(tab['First author']):
+        if " for " in f:
+            a, b = f.split(' for ')
+            tab['First author'][i] = a
+            team[i] = ' for ' + b
+    tab['team'] = team
+    for i, t in enumerate(tab['team']):
+        if t != '':
+            tab['authorlist'][i][0] = tab['authorlist'][i][0].split(' for ')[0] + ' (1)'
+            tab['affiliations'][i] = '(1) ' + tab['affiliations'][i]
+
+    # Poster submissions will always be accepted as poster unless specifically
+    # marked otherwise
+    # Want to edit col, but might be too small to fit the string,
+    # so make new col first
+    newtype = Column(tab['typepreference'], dtype='<U20')
+    if kwargs['autoacceptposters']:
+        newtype[(newtype == '') & (tab['typepreference'] == 'poster')] = 'poster'
+        tab['type'] = newtype
+
+    # Now some checks
+    ind_poster = tab['type'] == 'poster'
+    posters = tab[ind_poster]
+
+    # check it's a number otherwise sort will fail because string sorting will
+    # give different answers
+    if not np.issubdtype(posters['poster number'].dtype, np.integer):
+        print('Poster numbers are not integers - they might be sorted randomly.')
+    # check that no two posters have the same number
+    unique_numbers, unique_counts = np.unique(posters['poster number'],
+                                              return_counts=True)
+    if (unique_counts > 1).sum() > 0:
+        print('The following poster numbers are used more than once:')
+        for i in (unique_counts > 1).nonzero()[0]:
+            print('{}  is assigned to {} posters'.format(unique_numbers[i],
+                                                         unique_counts[i]))
+
+
+def read_abstracts_table(filename, **kwargs):
+    out = []
+    with open(filename, newline='') as f:
+         reader = csv.reader(f)
+         for row in reader:
+             out.append(row)
+    abstr = Table(rows=out[1:], names=out[0])
+    abstr = abstr[abstr['Timestamp'] != '']
+    process_google_form_value(abstr, **kwargs)
+    return abstr
+
+
+def data(**kwargs):
+    if kwargs['abstracts'] is None:
+        write_json_abstracts([])
+        return {'talks': [], 'posters': [], 'unassigned': []}
+    # abstr = Table.read(abstrfile, fast_reader=False, fill_values=())
+    abstr = read_abstracts_table(kwargs['abstracts'], **kwargs)
+    abstr['Email Address'] = [s.lower() for s in abstr['Email Address']]
+
+    if kwargs['registered_abstracts']:
+        regabs = Table.read(kwargs['registered_abstracts'], format='ascii.csv')
+        #regabs['registered'] = True
+        abstr = join(abstr, regabs)
+        #abstr = abstr[abstr['registered']]
+
+
+
+    ind_talk = (abstr['type'] == 'invited') | (abstr['type'] == 'contributed')
+    ind_poster = abstr['type'] == 'poster'
+
+    talks = abstr[ind_talk]
+    talks.sort(['binary_time', 'type', 'Select a major science topic'])
+    posters = abstr[ind_poster]
+    posters['intnumber'] = [int(i) for i in posters['poster number']]
+    posters.sort(['intnumber', 'Authors'])
+
+    # List all entries that do not have a valid type
+    notype = abstr[~ind_talk & ~ind_poster]
+
+    if len(notype) > 0:
+        print('The following entries do not have a valid "type" entry, which would classify them as talk or poster:')
+        for r in notype:
+            print(r['Timestamp'], r['type'], r['Title'])
+
+    if not kwargs['output_unassigned']:
+        abstr = abstr[abstr['type'] != '']
+    abstr.sort(['binary_time', 'poster number'])
+    abstr['index'] = np.arange(1.0 * len(abstr))
+    write_json_abstracts(abstr)
+
+    #notype.sort(['Select a major science topic', 'Authors'])
+    unass = notype if kwargs['output_unassigned'] else []
+
+    return {'talks': talks, 'posters': posters, 'unassigned': unass}
